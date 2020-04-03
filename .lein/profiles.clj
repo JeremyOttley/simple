24341b9dc87afc2f8b7b2a77445dce55eb1e5aed
Jeremy Ottley <Gazaunga@gmail.com>
2020-04-03 12:25:02 -0400

    Create profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,12 @@
+{:user {:dependencies [[org.clojure/tools.namespace "0.2.3"]]
+        :injections [(require '(clojure.tools.namespace repl find))]
+        :plugins [[lein-pprint "1.1.1"]
+                  [lein-clojars "0.9.1"]
+                  [lein-exec "0.3.1"]
+                  [cider/cider-nrepl "0.24.0"]
+                  [lein-ancient "0.6.15"]
+                  [lein-try "0.4.3"]
+                  [lein-asciidoctor "0.1.12"]
+                  [lein-kibit "0.1.8"]
+                  [venantius/ultra "0.6.0"]]
+        :middleware [cider-nrepl.plugin/middleware]}}
