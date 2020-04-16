2d1cbf76277e670ca87bfa58a38494331c57d739
Jeremy Ottley <Gazaunga@gmail.com>
2020-04-16 08:06:59 -0400

    Update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,23 +1,18 @@
-{:user {:dependencies [[org.clojure/tools.namespace "0.2.3"]]
-        :injections [(require '(clojure.tools.namespace repl find))]
-        :plugins [[lein-pprint "1.1.1"]
-                  [lein-clojars "0.9.1"]
-                  [lein-exec "0.3.1"]
+{:user {:dependencies [[org.clojure/tools.namespace "0.2.3"]
+                       [clj-http "3.10.0"]
+                       [cheshire "5.10.0"]]
+        :injections [(require '[clojure.tools.namespace.repl :refer [refresh]]
+                              '[clj-http.client :as http]
+                              '[cheshire.core :refer :all]
+                              '[clojure.edn :as edn])]
+        :plugins [[lein-try "0.4.3"]
+                  [jonase/eastwood "0.3.10"]
+                  [lein-kibit "0.1.8"]
                   [cider/cider-nrepl "0.24.0"]
                   [lein-ancient "0.6.15"]
-                  [lein-try "0.4.3"]
-                  [lein-asciidoctor "0.1.12"]
-                  [lein-kibit "0.1.8"]
-                  [venantius/ultra "0.6.0"]]
-        :asciidoctor {:sources "doc/*.ascii"
-              :format :html5
-              :to-dir "out/"
-              :header false
-              :toc :left
-              :toc-title "Contents"
-              :doctype :article
-              :source-highlight true}
-        :repl
-               {:plugins [[cider/cider-nrepl "0.21.1"]
-               :dependencies [[nrepl "0.4.5"]]}
-        :repl-options {:prompt (fn [ns] (format "%s(%s) => " ns (System/getenv "HOME")))}}}
+                  [venantius/ultra "0.6.0"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.24.0"]]
+        :dependencies [[nrepl "0.4.5"]]
+        :repl-options {:timeout 150000
+                       :prompt (fn [ns] (format "%s(%s) => " ns (System/getenv "HOME")))}}}
+
